ca7d1cf17b215d4c58568314def0f0ab3b109ae2
Vishal Mahale <vmahale799@gmail.com>
2023-12-21 15:22:47 +0530

    first commit

---
+++ js/slider.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function($) {
+    "use strict";
+    //  TESTIMONIALS CAROUSEL HOOK
+    $('#customers-testimonials').owlCarousel({
+        loop: true,
+        center: true,
+        margin:-38,		           
+        dots:true,
+        autoplayTimeout: 5000,
+        smartSpeed: 450,
+        responsive: {
+          0: {
+            items: 1
+          },
+          768: {
+            items: 2
+          },
+          1170: {
+            items: 3
+          }
+        }
+    });
+});
